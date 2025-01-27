4ff121e91180264096e8ba3df60ffbce76748b7c
Aleida Vieyra <avieyra@ufl.edu>
2025-01-27 15:05:35 -0500

    added landlord registration initial form, and component, as well as it's css styling' its

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { useState } from 'react'
 
+import Register from './components/login_register/Register';
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Optional, for components like modals or dropdowns
+
 
 
 
 function App() {
 
-	return <div className="App"></div>
+	return <div className="App">
+		<Register />
+	</div>
 
 
 }
--- src/components/login_register/Register.tsx
+++ src/components/login_register/Register.tsx
@@ -1,9 +1,38 @@
 
-import React from 'react';
+import React from 'react'
+import './Register.css';
 
 
 
 const Register = () => {
 
-	
+	return (
+		<div className = 'addLandlord'>
+			<h3>Landlord Registration</h3>
+			<form className='addLandlordForm'>
+				<div className='landlordInput'>
+					<label htmlFor='name'> Username </label>
+					<input
+					type='text'
+					id='name'
+					autoComplete='off'
+					placeholder='Enter username'
+					>  
+					</input>
+				
+				
+					<label htmlFor='name'> Password </label>
+					<input
+						type='password'
+						id='password'
+						autoComplete='off'
+						placeholder='Enter password'
+					>  
+					</input>
+				</div>
+			</form>
+		</div>
+	)
 }
+
+export default Register;
