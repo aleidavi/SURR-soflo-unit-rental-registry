fee495f0ddf9d19beb815804b2e8e1d77b1f672f
Aleida Vieyra <avieyra@ufl.edu>
2025-01-27 13:15:02 -0500

    initial React-Typescript SURR Front-end file structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-const name: string = 'Aleida';
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 function App() {
- 
+
+	return <div className="App"></div>
 
 
- 
 }
  
  
---
+++ src/components/property/Property.tsx
@@ -0,0 +1,2 @@
+import '../../App.tsx';
+import './PropertyList.tsx';
