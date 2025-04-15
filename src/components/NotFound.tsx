390f8fb60aade2af640339928f6a26c3314514a4
aleidavi <avieyra@ufl.edu>
2025-04-15 15:00:13 -0400

    adding a NotFound page/component

---
+++ src/components/NotFound.tsx
@@ -0,0 +1,14 @@
+
+
+function NotFound(){
+
+    return <div> 
+        <h1>404 NOT FOUND</h1> 
+        <p>Sorry, the page you are looking for does not exist.</p>
+        <p>Please check the URL or return to the homepage.</p>
+    </div>
+
+    
+}
+
+export default NotFound;
